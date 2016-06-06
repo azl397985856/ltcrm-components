ceffdf83708a017b16cf5f661849a8e38c43e2f0
azl397985856 <695694307@qq.com>
2016-06-06 14:12:09 +0800

    delete the useless folder and files

--- .storybook/config.js
+++
@@ -1,8 +0,0 @@
-import { configure } from '@kadira/storybook';
-import '../style/layout.css';
-
-function loadStories() {
-  require('../stories/');
-}
-
-configure(loadStories, module);
--- stories/index.js
+++
@@ -1,9 +0,0 @@
-import 'antd/lib/index.css';
-import './ltDragable.js';
-import './ltTable.js';
-import './ltSelect.js';
-import './imgZoom.js';
-import './disabledDatePicker.js'
-import './checkBoxGroup';
-import './progress';
-import './ltWizard'
